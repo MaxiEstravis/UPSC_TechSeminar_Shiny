5e5bd3a94767f66e5774a0ebc1fcd491ac8f270c
MaxiEstravis <maxiestravis@gmail.com>
2024-05-20 15:07:24 +0200

    smaller font, notes, basic reactivity

--- examples/01_output-types.r
+++ examples/01_output-types.r
@@ -5,15 +5,22 @@ shinyApp(
     titlePanel("Example - Different output types"),
     sidebarLayout(
       sidebarPanel(
-        textInput("text_output",label="Text input", value="Type text here")
+        textInput("text_output",
+                  label="Text input", 
+                  value="Type text here"),
+        selectInput("select_align", 
+                    label="Select column alignment", 
+                    choices=c("l","c","r"))
       ),
       mainPanel(
         verbatimTextOutput("verbatim_text_output"),
         tableOutput("table_output"),
         plotOutput("plot_output",width="400px",height="400px")))),
   server=function(input, output) {
-    output$text_output <- renderText({input$text_output})
     output$verbatim_text_output <- renderText({input$text_output})
-    output$table_output <- renderTable({mtcars[1:10,1:6]})
-    output$plot_output <- renderPlot({plot(mtcars[,1],mtcars[,6],main = input$text_output)})
+    output$table_output <- renderTable({mtcars[1:10,1:6]}, 
+                                        align = reactive({input$select_align}), 
+                                        width = "200%")
+    output$plot_output <- renderPlot({plot(mtcars[,1],mtcars[,6], main = input$text_output)}, 
+                                     width = 1000)
   })
